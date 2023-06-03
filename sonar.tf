9002e269ed528ba2b51281b26dcd6351e617d9db
mikeodejayi <122144928+mikeodejayi@users.noreply.github.com>
2023-06-03 04:16:40 -0500

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -40,11 +40,3 @@ resource "aws_instance" "mySonarInstance" {
       }
     }
 
-# Create Elastic IP address for Sonar instance
-resource "aws_eip" "mySonarInstance" {
-  vpc      = true
-  instance = aws_instance.mySonarInstance.id
-tags= {
-    Name = "sonar_elastic_ip"
-  }
-}
