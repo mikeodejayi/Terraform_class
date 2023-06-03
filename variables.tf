b29046ab370955996c2d253429d162534ae82b1d
mikeodejayi <122144928+mikeodejayi@users.noreply.github.com>
2023-06-03 02:00:57 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2021" 
+    default     = "jenkins-sgroup-mike-2023" 
 }
 
 variable "tag_name" { 
